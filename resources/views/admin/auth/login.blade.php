73e4507a00f2241d2a7d64b047ef291dedec3fd9
yusei0211 <saito31126@icloud.com>
2022-09-24 15:55:58 +0900

    logo_set

--- resources/views/admin/auth/login.blade.php
+++ resources/views/admin/auth/login.blade.php
@@ -1,10 +1,12 @@
 <x-guest-layout>
-    アドミン用
     <x-auth-card>
+        アドミン用
         <x-slot name="logo">
-            <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-            </a>
+            <div class="w-28">
+                <a href="/">
+                    <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+                </a>
+            </div>
         </x-slot>
 
         <!-- Session Status -->
--- resources/views/owner/auth/login.blade.php
+++ resources/views/owner/auth/login.blade.php
@@ -1,6 +1,6 @@
 <x-guest-layout>
-    オーナー用
     <x-auth-card>
+         オーナー用
         <x-slot name="logo">
             <a href="/">
                 <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
