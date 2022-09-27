f9d0738835a9d0ce9b3facffaaed66ba2e16c3f8
yusei0211 <saito31126@icloud.com>
2022-09-27 10:58:10 +0900

    ownersDelete完成

--- app/Http/Controllers/Admin/OwnersController.php
+++ app/Http/Controllers/Admin/OwnersController.php
@@ -130,6 +130,6 @@ class OwnersController extends Controller
      */
     public function destroy($id)
     {
-        //
+        dd('削除処理');
     }
 }
--- resources/views/admin/owners/index.blade.php
+++ resources/views/admin/owners/index.blade.php
@@ -25,6 +25,7 @@
                                 <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">メールアドレス</th>
                                 <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">作成日</th>
                                 <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tr rounded-br"></th>
+                                <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tr rounded-br"></th>
                               </tr>
                             </thead>
                             <tbody>
@@ -36,6 +37,13 @@
                                 <td class="px-4 py-3">
                                   <button onclick="location.href='{{ route('admin.owners.edit',['owner' => $owner->id ]) }}'" class="text-white bg-blue-400 border-0 py-2 px-4 focus:outline-none hover:bg-indigo-500 rounded ">編集する</button>
                                 </td>
+                                <form id="delete_{{$owner->id}}" method="post" action="{{ route('admin.owners.destroy',['owner' => $owner->id ]) }}">
+                                  @csrf
+                                  @method('delete')
+                                  <td class="px-4 py-3">
+                                    <a href = "javascript:void(0);" data-id="{{ $owner->id }}" onclick="deletePost(this)" class="text-white bg-red-400 border-0 py-2 px-4 focus:outline-none hover:bg-red-500 rounded ">削除</a>
+                                  </td>
+                                </form>
                               </tr>
                               @endforeach
                             </tbody>
@@ -59,4 +67,12 @@
             </div>
         </div>
     </div>
+    <script>
+      function deletePost(e) {
+        'use strict';
+        if (confirm('本当に削除してもいいですか？')) {
+          document.getElementById('delete_' + e.dataset.id).submit();
+        }
+      }
+    </script>
 </x-app-layout>
