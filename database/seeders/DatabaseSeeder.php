f450dc3f9557ab9fc9ecea654184cbe948bfc9c6
yusei0211 <saito31126@icloud.com>
2022-11-03 00:49:29 +0900

    image seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,6 +19,7 @@ class DatabaseSeeder extends Seeder
                 AdminSeeder::class,
                 OwnerSeeder::class,
                 ShopSeeder::class,
+                ImageSeeder::class
             ]);
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
--- database/seeders/ShopSeeder.php
+++ database/seeders/ShopSeeder.php
@@ -2,10 +2,8 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Hash;
 
 class ShopSeeder extends Seeder
 {
@@ -17,20 +15,20 @@ class ShopSeeder extends Seeder
     public function run()
     {
         DB::table('shops')->insert([
-                [
+            [
                 'owner_id' => 1,
                 'name' => 'ここに店名が入ります',
-                'information' => 'ここにお店の情報が入ります',
+                'information' => 'ここにお店の情報が入ります。ここにお店の情報が入ります。ここにお店の情報が入ります。',
                 'filename' => '',
-                'is_selling'=> true
-                ],
-                [
+                'is_selling' => true    
+            ],
+            [
                 'owner_id' => 2,
                 'name' => 'ここに店名が入ります',
-                'information' => 'ここにお店の情報が入ります',
+                'information' => 'ここにお店の情報が入ります。ここにお店の情報が入ります。ここにお店の情報が入ります。',
                 'filename' => '',
-                'is_selling'=> true
-                ],
-                ]);
+                'is_selling' => true    
+            ],
+            ]);
     }
 }
